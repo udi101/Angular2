80f5a30f826544a0eb2eb5ee2d2172aacb68f096
udi101 <udimaz@gmail.com>
2017-03-12 10:39:04 +0200

    12.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private router: Router, private authService:AuthService) { }
+
+  checkActive(): boolean {
+    return (this.router.url.startsWith('/User'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,86 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+
+
+//services
+import { WorkerService } from './core/worker.service';
+import { AuthService } from './user/auth.service';
+
+//  Components
+import {
+  GrandFatherComponent,
+  FatherComponent,
+  GrandchildComponent
+} from './grandfather/index';
+
+import {
+  WorkerContainerComponent,
+  WorkerListComponent
+} from './workers/index'
+
+// Guards
+import { RouteGuardService } from './core/routeGuard.service';
+import { P404Component } from './404/p404.component';
+import { FoodComponent } from './food/food.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkerContainerComponent,
+    WorkerListComponent,
+    GrandFatherComponent,
+    FatherComponent,
+    GrandchildComponent,
+    P404Component,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'Grandfather', component: GrandFatherComponent, canActivate: [RouteGuardService] },
+      {
+        path: 'Worker', component: WorkerContainerComponent,
+        canActivate: [RouteGuardService],
+        canDeactivate: ['canDeactivateWorker'],
+        resolve: { workers: RouteGuardService }
+      },
+      {
+        path: 'Worker/:id', component: WorkerContainerComponent,
+        canActivate: [RouteGuardService],
+        resolve: {workers : RouteGuardService}
+      },
+      { path: 'Food', component: FoodComponent },
+      { path: 'User', loadChildren: 'app/user/user.module#UserModule' },   // טעינת ניווט משני
+      { path: '', component: WorkerContainerComponent, canActivate: [RouteGuardService] },
+      { path: '**', component: P404Component, canActivate: [RouteGuardService] }
+    ])
   ],
-  providers: [],
+  providers: [
+    RouteGuardService,
+    AuthService,
+    WorkerService,
+    {
+      provide: 'canDeactivateWorker',
+      useValue: canDeactivateWorkerFunction
+    },
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+
+function canDeactivateWorkerFunction(component: WorkerContainerComponent): boolean {
+  if (component.isDirty)  // מאפשר יציאה  
+    return window.confirm("Are you sure you want to exit?");
+  else
+    return true;
+}
+
